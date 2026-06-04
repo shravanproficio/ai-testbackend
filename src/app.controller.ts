9a9a41855b69ceea55dd38dd0b6d9f748ed63e35
shravanproficio <shravan.a@proficio.in>
2026-06-04 16:55:37 +0530

    second commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body
+} from '@nestjs/common';
+
+import axios from 'axios';
+
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+
+  constructor(
+    private readonly appService: AppService
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('chat')
+  async chat(
+    @Body() body: { message: string }
+  ) {
+
+    const response = await axios.post(
+      'https://commute-ai-demo.onrender.com/chat',
+      {
+        message: body.message
+      }
+    );
+
+    return response.data;
   }
 }
